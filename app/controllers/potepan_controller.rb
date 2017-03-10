42363a8c63a507cd0e18e4c3d2512856c961849d
Chihiro Shimmur <chihiros92@suou.waseda.jp>
2017-03-11 05:03:37 +0900

    Edit routes

--- app/controllers/potepan_controller.rb
+++ app/controllers/potepan_controller.rb
@@ -2,11 +2,6 @@ class PotepanController < ApplicationController
 
   #def index
   #end
-  def single_product
-    @product = Spree::Product.first
-    # @product = Spree::Product.find(params[:id])
-  end
-
   def checkout_complete
   end
   def checkout_step_1
@@ -14,6 +9,10 @@ class PotepanController < ApplicationController
   def checkout_step_3
   end
   def checkout_step_4
+  end
+
+  def single_product
+    @single_product = Spree::Product.first
   end
 
 end
