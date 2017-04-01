661c1df2a397bf874b86744977b490654353a4d3
chicn <chihiros92@suou.waseda.jp>
2017-04-01 10:57:06 +0900

    Show all prodicts at index page

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,11 +2,18 @@ class Potepan::ProductsController < ApplicationController
 
   def index
     @prototypes = Spree::Prototype.order('name asc')
+    @products = Spree::Product.all
   end
 
   def show
     @products = Spree::Product.find_by(params[:id])
     @image = @products.display_image
   end
+
+  def show_category
+
+
+  end
+
 
 end
