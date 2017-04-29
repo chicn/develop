74f9aaae497e3c3bc4f3cec4fc59ef94e4005df2
chicn <chihiros92@suou.waseda.jp>
2017-04-29 10:43:36 +0900

    categoryごとのproductsを取れない、、

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -3,16 +3,18 @@ class Potepan::ProductsController < ApplicationController
   def index
     @categories = Spree::Prototype.order('name asc')
     if params.has_key?("category")
-      temp = Spree::Product.all
-      @products = []
-      temp.each do |t|
-        if t.taxons[0].prototypes[0].id.to_s == params[:category]
-          @products << temp
-        end
-      end
-      # @products = Spree::Product.all
+      # temp = Spree::Product.all
+      # @products = []
+      # temp.each do |t|
+      #   if t.taxons[0].prototypes[0].id.to_s == params[:category]
+      #     @products << temp
+      #   end
+      # end
+binding.pry
+      @products = Spree::Product.includes(:prototypes).where("spree_prototypes.id = ?", params[:category]).references(:prototypes)
     else
       @products = Spree::Product.all
+      binding.pry
     end
   end
 
